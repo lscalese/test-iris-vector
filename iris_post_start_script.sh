b7a6e8fe1e4b0add4bbe2bda3e954c90c0c172c4
Lorenzo Scalese <lorenzo.scalese@gmail.com>
2024-08-11 11:53:14 +0200

    escape $ character

--- iris_post_start_script.sh
+++ iris_post_start_script.sh
@@ -3,9 +3,9 @@
 # Add your own commands to this file to customize the startup of your IRIS instance.
 iris session $ISC_PACKAGE_INSTANCENAME -U IRISAPP <<- END
 zpm "install swagger-ui"
-Do $SYSTEM.OBJ.LoadDir("/home/irisowner/dev/src/","ck", , 1)
+Do \$SYSTEM.OBJ.LoadDir("/home/irisowner/dev/src/","ck", , 1)
 Set sc = ##class(Ens.Director).StartProduction("careia.production.careia")
-Write !,"Production Start Status: ", $SYSTEM.Status.GetOneErrorText(sc), !
+Write !,"Production Start Status: ", \$SYSTEM.Status.GetOneErrorText(sc), !
 Halt
 END
 
